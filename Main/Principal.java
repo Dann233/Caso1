594668d106860edf2d9db378995f2fcbdc822d33
Dann233 <ds005110@gmail.com>
2024-04-25 14:19:14 -0600

    Se realizó un metodo para sumar 2 variables

--- Main/Principal.java
+++ Main/Principal.java
@@ -1,9 +1,25 @@
+import javax.swing.JOptionPane;
+
 public class Principal {
 
+
+    static int  total = 0;
+  static  int a = 0;
+  static  int b = 0;
     public static void main(String[] args) {
 
-        System.out.println("Bienvenido a sumar dos numeros...");
+JOptionPane.showMessageDialog(null, "Bienvenido a sumar dos numeros...");
 
+        
+        
+        sumatoria();
+
+    }
+
+
+    public static void sumatoria (){
+        total = a + b;
+        System.out.println("El resultado de suma es de: "+total);
     }
 
 }
